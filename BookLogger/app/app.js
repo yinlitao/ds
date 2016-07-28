b15f417867d621c492f723827922934c188167f1
litao-mac <litao.yin@gmail.com>
2016-07-28 06:35:13 -0500

    second version

--- BookLogger/app/app.js
+++ BookLogger/app/app.js
@@ -15,16 +15,31 @@
         };
       };
     });
-
-    $provide.provider("testService", function(){
-      this.$get=function(){
-        var serviceName = 'test Service';
-
-        return{
-          serviceName : serviceName
-        }
-      };
-    });
-
   });
+
+  //actually, the service will be added provider at the end of the service name
+  app.provider("testService", function(){
+    this.$get=function(){
+      var serviceName = 'test Service';
+      var serviceVersion = '1.0';
+
+      if(includeVersionInTitle){
+        appName  += ' ' + serviceVersion;
+      }
+
+      return{
+        serviceName : serviceName
+      }
+    };
+
+    var includeVersionInTitle = false;
+    this.setIncludeVersionInTitle = function(value){
+        includeVersionInTitle = value;
+    };
+  });
+
+  app.config(function(testServiceProvider){
+    testServiceProvider.setIncludeVersionInTitle(true);
+  });
+
 }());
