15d76f212fabb7ea7c99e97d5be19878d07402c3
litao-mac <litao.yin@gmail.com>
2016-07-28 23:12:37 -0500

    service factory class

--- BookLogger/app/books/booksController.js
+++ BookLogger/app/books/booksController.js
@@ -4,9 +4,12 @@
 
   //this is the constructor, where is the books coming from?
   //oh, from the app.js, name convention
-  function booksController(books, testService){
+  function booksController(books, testService, dataService, logger){
     var vm = this;
     vm.appName = books.appName;
     vm.serviceName = testService.serviceName;
+    vm.allBooks = dataService.getAllBooks();
+
+    logger.output('booksController as been created');
   }
 }());
