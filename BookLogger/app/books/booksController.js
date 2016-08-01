765678b06312f35439367ba40066e2ac70f4e70c
litao-mac <litao.yin@gmail.com>
2016-07-31 22:42:33 -0500

    working version with $q

--- BookLogger/app/books/booksController.js
+++ BookLogger/app/books/booksController.js
@@ -8,10 +8,52 @@
     var vm = this;
     vm.appName = books.appName;
     vm.serviceName = testService.serviceName;
-    vm.allBooks = dataService.getAllBooks();
-    vm.allReaders = dataService.getAllReaders();
+    //vm.allBooks = dataService.getAllBooks();
+    dataService.getAllBooks()
+      .then(getBooksSuccess, null, getBooksNotification)
+      .catch(errorCallback)
+      .finally(getAllBooksComplete);
+
+    //vm.allReaders = dataService.getAllReaders();
+
+    dataService.getAllReaders()
+      .then(getAllReadersSuccess)
+      .catch(errorCallback)
+      .finally(getAllReadersComplete);
+
+
+    function getAllReadersSuccess(readers){
+      vm.allReaders = readers;
+    }
+
+    function getAllReadersComplete(){
+      console.log('getAllReaders has completed');
+    }
+
+    
     vm.getBadge = badgeService.retrieveBadge;
 
     logger.output('booksController as been created');
+
+    function getBooksSuccess(books){
+      vm.allBooks = books;
+    }
+
+    function getBooksError(reason){
+      console.log(reason);
+    }
+
+    function errorCallback(message){
+      console.log('Error Message: ' + message);
+    }
+
+    function getAllBooksComplete(){
+      console.log('getAllBooks has completed');
+    }
+
+    function getBooksNotification(notification){
+      console.log('Promise notification : ' + notification);
+
+    }
   }
 }());
--- BookLogger/app/services/dataService.js
+++ BookLogger/app/services/dataService.js
@@ -1,57 +1,85 @@
 (function(){
   angular.module('app')
-    .factory('dataService', dataService);
+    .factory('dataService',['$q', '$timeout', dataService]);
 
-    function dataService(){
+    function dataService($q, $timeout){
         return {
           getAllBooks:getAllBooks,
           getAllReaders:getAllReaders
         };
-    };
 
-    function getAllBooks(){
-      return [
-        {
-          book_id: 1,
-          title: 'Harry Potter and the Deathly Hallows',
-          author: 'J.K. Rowling',
-          year_published: 2000
-        },
-        {
-          book_id:2,
-          title:'The Cat in the Hat',
-          author:'Dr. Seuss',
-          year_published: 1957
-        },
-        {
-          book_id: 3,
-          title: 'Encyclopedia Brown, Boy Detective',
-          author: 'Donald J. Sobol',
-          year_published: 1963
-        }
-      ];
-    }
 
-    function getAllReaders(){
-      return [
-        {
-          reader_id: 1,
-          name: 'Marie',
-          weeklyReadingGoal:315,
-          totalMinutesRead:5600
-        },
-        {
-          reader_id:2,
-          name:'Daniel',
-          weeklyReadingGoal:210,
-          totalMinutesRead:3000
-        },
-        {
-          reader_id: 3,
-          name:'Lanier',
-          weeklyReadingGoal: 140,
-          totalMinutesRead: 600
-        }
-      ];
-    };
+      function getAllBooks(){
+        var booksArray= [
+          {
+            book_id: 1,
+            title: 'Harry Potter and the Deathly Hallows',
+            author: 'J.K. Rowling',
+            year_published: 2000
+          },
+          {
+            book_id:2,
+            title:'The Cat in the Hat',
+            author:'Dr. Seuss',
+            year_published: 1957
+          },
+          {
+            book_id: 3,
+            title: 'Encyclopedia Brown, Boy Detective',
+            author: 'Donald J. Sobol',
+            year_published: 1963
+          }
+        ];
+
+        var deferred = $q.defer();
+
+        $timeout(function(){
+          var successful = true;
+          if(successful){
+
+            deferred.notify('Just gettting started gathering books...');
+            deferred.notify('Almost done gathering books...');
+
+            deferred.resolve(booksArray);
+          }
+          else{
+            deferred.reject('Error retrieving books.');
+          }
+
+        }, 1000);
+
+        return deferred.promise;
+      }
+
+      function getAllReaders(){
+        var readerArray= [
+          {
+            reader_id: 1,
+            name: 'Marie',
+            weeklyReadingGoal:315,
+            totalMinutesRead:5600
+          },
+          {
+            reader_id:2,
+            name:'Daniel',
+            weeklyReadingGoal:210,
+            totalMinutesRead:3000
+          },
+          {
+            reader_id: 3,
+            name:'Lanier',
+            weeklyReadingGoal: 140,
+            totalMinutesRead: 600
+          }
+        ];
+
+        var deferred = $q.defer();
+
+        $timeout(function(){
+          deferred.resolve(readerArray);
+        }, 1500);
+
+        return deferred.promise;
+      };
+  };
 }());
